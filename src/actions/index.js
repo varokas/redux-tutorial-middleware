723cae15ed19e57c000e14d3c8468e8c6161b121
Varokas Panusuwan <varokas@panusuwan.com>
2017-04-09 10:22:59 +0700

    add actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '04ba78e87670379a4118a7903e691577';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
